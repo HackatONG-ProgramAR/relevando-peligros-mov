ab1dbc11502075fc6d9a35f29af815e76db5c488
stamagnoglb <santiago.tamagno@globant.com>
2014-09-05 18:02:40 -0300

    Add take a picture

--- app/src/main/java/org/relevandopeligros/activity/RegistrarPeligroActivity.java
+++ app/src/main/java/org/relevandopeligros/activity/RegistrarPeligroActivity.java
@@ -3,7 +3,9 @@ package org.relevandopeligros.activity;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,6 +14,9 @@ import android.view.ViewGroup;
 import android.os.Build;
 
 import org.relevandopeligros.relevandopeligrosapp.R;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class RegistrarPeligroActivity extends Activity {
 
@@ -56,9 +61,27 @@ public class RegistrarPeligroActivity extends Activity {
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
+                                 Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_registrar_peligro, container, false);
+            ButterKnife.inject(this, rootView);
             return rootView;
         }
+
+        static final int REQUEST_IMAGE_CAPTURE = 1;
+
+        @OnClick(R.id.take_picture)
+        void dispatchTakePictureIntent() {
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+
+        @Override
+        public void onDestroyView() {
+            super.onDestroyView();
+            ButterKnife.reset(this);
+        }
     }
+
 }
